0358202a0f304cbb3eb06dc1b99d6fed0cd4fd00
FreddyV <freddyv@gmail.com>
2017-10-23 17:08:55 +0200

    conversie experimentjes

--- javascript/Oefeningen/fv_oef01.js
+++ javascript/Oefeningen/fv_oef01.js
@@ -9,4 +9,5 @@ var getal2 = readlineSync.question('Type het tweede getal in:');
 console.log('Het product van %s en %s is', getal1, getal2, parseInt(getal1) * parseInt(getal2)); // no conversion needed
 console.log('Het product van %s en %s is', getal1, getal2, getal1 * getal2);
 console.log('Het product van', getal1, 'en', getal2, 'is', getal1 * getal2);
-
+console.log("som: %d", getal1 + getal2); // string concat
+console.log('som: %s', getal1+getal2);  // string concat
