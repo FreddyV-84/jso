68599f379754ff4c2fd499eebbb1f50ac71329db
FreddyV <freddyv@gmail.com>
2017-10-23 16:04:19 +0200

    vandaag nog brol.

--- javascript/basisstructuren/typesWijzigen.js
+++ javascript/basisstructuren/typesWijzigen.js
@@ -9,15 +9,17 @@ console.log(x/y);  // 3.3333333333333335
 console.log(x+y);  // 13
 
 y = "3";
-console.log(x/y);  // 3.3333333333333335
-console.log(x+y);  // 103
+console.log(x/y);  // 3.3333333333333335 --> IMPLICIT CAST to floating number
+console.log(x+y);  // 103 --> string concatenation
  
  // string converteren naar integer (1)
-z = parseInt(y);  // z = 3 
+z = parseInt(y);  // z = 3 --> EXPLICIT CAST to Int
 console.log(x+z);   // 13
 
 y = "1";
 console.log(x+y);   // 101
 // string converteren naar integer (2)
-z = +y;             // z = 1
+z = +y;             // z = 1 --> shorthand notation for EXPLICIT CAST to Int
 console.log(x+z);   // 11
+
+// + als er links of rechts van de plus een string staat wordt de operatie geconcateneerd.
