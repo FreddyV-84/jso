1c34eeb52d821ed879377c1c1d3bfc35a63d4b79
FreddyV <freddyv@gmail.com>
2017-10-24 11:52:29 +0200

    switch statement gebruikt

--- javascript/Oefeningen/fv_OefSwitch02.js
+++ javascript/Oefeningen/fv_OefSwitch02.js
@@ -1,11 +1,11 @@
 'use strict'
-
 var input = require('readline-sync');
 
-var puntenStudent = input.question("Geef de punten van de student in (0 - 20):");
 var graad;
+var puntenStudent = input.question("Geef de punten van de student in (0 - 20):");
 
-if(puntenStudent < 10)
+
+/* if(puntenStudent < 10)
     graad = "onvoldoende";
 else if(puntenStudent < 14)
     graad = "voldoende";
@@ -16,6 +16,26 @@ else if(puntenStudent < 18)
 else if (puntenStudent <= 20)
     graad = "grootste onderscheiding";
 else
-    graad = "ongeldige score";
+    graad = "ongeldige score"; */
+
+switch (true){
+    case (puntenStudent >= 0 && puntenStudent < 10):
+        graad = "onvoldoende";
+        break;
+    case (puntenStudent >= 10 && puntenStudent < 14):
+        graad = "voldoende";
+        break;
+    case (puntenStudent >= 14 && puntenStudent < 16):
+        graad = "onderscheiding";
+        break;
+    case (puntenStudent >= 16 && puntenStudent < 18):
+        graad = "grote onderscheiding";
+        break;
+    case (puntenStudent <= 18 && puntenStudent <= 20):
+        graad = "grootste onderscheiding";
+        break;
+    default:
+        graad = "ongeldige score";
+}
 
 console.log(graad);
