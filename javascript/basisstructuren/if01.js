72b5e35ba8060214b84b42b98a7c7475d94414da
FreddyV <freddyv@gmail.com>
2017-10-24 10:02:03 +0200

    Added comparisonOperators oefening uitgebreid added comments

--- javascript/basisstructuren/if01.js
+++ javascript/basisstructuren/if01.js
@@ -2,8 +2,19 @@
 
 var datum = new Date();
 var uur = datum.getHours();
+
+var minuten = datum.getMinutes();
 if (uur < 12) {
-	console.log("Goedemorgen");
+	console.log("Goedemorgen");		// if-block
 } else {
-	console.log("Goedenamiddag");
+	console.log("Goedenamiddag");	// else-block
 }
+
+if(uur >= 9 && uur <=12)
+	console.log("de werkvoormiddag is bezig");
+
+if(uur < 9 || uur > 18)
+	console.log("Buiten de werkuren");
+
+console.log("Het is " + uur + ":" + minuten + " uur.");
+console.log(datum);
--- javascript/basisstructuren/rekenkundig.js
+++ javascript/basisstructuren/rekenkundig.js
@@ -12,9 +12,9 @@ som += x;
 console.log(som);
 som++;
 console.log(som);
-console.log(som++);
+console.log(som++); // postfix increment:   first returns value of variable, then increments variable
 console.log(som);
-console.log(++som);
+console.log(++som); // prefix increment:    first increments variable, then returns value of incremented variable
 console.log(som);
 
 resultaat1 = x + y * z;
