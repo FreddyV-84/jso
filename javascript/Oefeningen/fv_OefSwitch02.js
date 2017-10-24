7b14c424fde3c18194975148ecf19c27ccf7abdd
FreddyV <freddyv@gmail.com>
2017-10-24 12:02:09 +0200

    added new switch

--- javascript/Oefeningen/fv_OefSwitch02.js
+++ javascript/Oefeningen/fv_OefSwitch02.js
@@ -2,7 +2,7 @@
 var input = require('readline-sync');
 
 var graad;
-var puntenStudent = input.question("Geef de punten van de student in (0 - 20):");
+var puntenStudent = parseInt(input.question("Geef de punten van de student in (0 - 20):"));
 
 
 /* if(puntenStudent < 10)
@@ -18,20 +18,46 @@ else if (puntenStudent <= 20)
 else
     graad = "ongeldige score"; */
 
-switch (true){
-    case (puntenStudent >= 0 && puntenStudent < 10):
-        graad = "onvoldoende";
-        break;
-    case (puntenStudent >= 10 && puntenStudent < 14):
+/* graad = "onvoldoende";
+switch(puntenStudent)
+{
+    case 10:
+    case 11:
+    case 12:
+    case 13:
         graad = "voldoende";
         break;
-    case (puntenStudent >= 14 && puntenStudent < 16):
+    case 14:
+    case 15:
         graad = "onderscheiding";
         break;
-    case (puntenStudent >= 16 && puntenStudent < 18):
+    case 16:
+    case 17:
         graad = "grote onderscheiding";
         break;
-    case (puntenStudent <= 18 && puntenStudent <= 20):
+    case 18:
+    case 19:
+    case 20:
+        graad = "grootste onderscheiding";
+        break;
+    default:
+        graad = "ongeldige score";
+} */
+
+switch (true){
+    case puntenStudent >= 0 && puntenStudent < 10:
+        graad = "onvoldoende";
+        break;
+    case puntenStudent >= 10 && puntenStudent < 14:
+        graad = "voldoende";
+        break;
+    case puntenStudent >= 14 && puntenStudent < 16:
+        graad = "onderscheiding";
+        break;
+    case puntenStudent >= 16 && puntenStudent < 18:
+        graad = "grote onderscheiding";
+        break;
+    case puntenStudent <= 18 && puntenStudent <= 20:
         graad = "grootste onderscheiding";
         break;
     default:
