b10d4e86a172257dc9fd7fff2561bdfe01ad743f
FreddyV <freddyv@gmail.com>
2017-10-24 12:25:43 +0200

    added comments

--- javascript/basisstructuren/specialekes.js
+++ javascript/basisstructuren/specialekes.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var smurf;
+var smurf; // = undefined
 
 console.log(1+1);  // 2
 console.log("1"+1); // 11
 console.log(2*"10");  // 20
 console.log(2*"smurf");  // NaN
-console.log(5/0);   // Infinity
+console.log(5/0);   // Infinity, is voor JS ook een getal
 console.log(smurf);   // undefined
 
 // we mogen (sleutelwoord) null toekennen aan een var
 // om aan te geven dat die var momenteel geen bruikbare waarde bevat
 smurf = null;
-console.log(smurf); // null
+console.log(smurf); // null (null pointer or null reference)
