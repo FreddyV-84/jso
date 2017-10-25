318962544039bfc6c9604bd3d02e62dc26117a7f
FreddyV <freddyv@gmail.com>
2017-10-25 11:40:24 +0200

    Added general function berekenMacht(grondtal, exponent)

--- javascript/basisstructuren/functies02.js
+++ javascript/basisstructuren/functies02.js
@@ -11,10 +11,23 @@ function berekenTweedeMacht(getal) {
     return resultaat;
 }
 
+// functie om een machtsverheffing te berekenen
+function berekenMacht(grondtal, exponent) {
+    var resultaat = 1;
+
+    for (var i = 1; i <= exponent; i++) {
+        resultaat *= grondtal;
+    }
+
+    return resultaat;
+}
+
 // functies oproepen ("gebruiken")
-getal1 = toetsenbord.question("Geef eerste getal: ");
-macht = berekenTweedeMacht(getal1) ;
+/* getal1 = toetsenbord.question("Geef eerste getal: ");
+macht = berekenTweedeMacht(getal1);
 console.log("Het getal %d vermenigvuldigd met zichzelf is %d", getal1, macht);
 getal2 = toetsenbord.question("Geef tweede getal: ");
 macht = berekenTweedeMacht(getal2);
-console.log("Het getal %d vermenigvuldigd met zichzelf is %d", getal2, macht);
+console.log("Het getal %d vermenigvuldigd met zichzelf is %d", getal2, macht); */
+
+console.log(berekenMacht(2, 0));
