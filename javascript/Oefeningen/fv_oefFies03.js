eb9110f08e97a1f6949e0566803b556735d3c3f9
FreddyV <freddyv@gmail.com>
2017-10-25 13:30:28 +0200

    function exercise 3 added

---
+++ javascript/Oefeningen/fv_oefFies03.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var input = require("readline-sync");
+var getal;
+
+/** Berekent het aantal graden celcius gegeven het aantal graden fahrenheit */
+function celsius(fGraden) {
+    return (5.0 / 9.0 * (fGraden - 32));
+}
+
+/** Berekent het aantal graden fahrenheit gegeven het aantal graden celsius */
+function fahrenheit(cGraden) {
+    return (9.0 / 5.0 * cGraden + 32);
+}
+
+getal = input.question("Geef een getal in: ");
+
+console.log(getal + " graden fahrenheit = " + celsius(getal) + " graden celcius");
+console.log(getal + " graden celsius = " + fahrenheit(getal) + " graden fahrenheit");
