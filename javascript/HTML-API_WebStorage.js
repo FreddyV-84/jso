3d18bf08ef1368dd88e49cc3176ff4593517fbb1
FreddyV <verheecke.frederik@gmail.com>
2017-12-04 10:37:02 +0100

    added overview of web storage html API

---
+++ javascript/HTML-API_WebStorage.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// HTML API: Web Storage
+// window.localStorage      - stores data with no expiration date
+// window.sessionStorage    - stores data for one session (data is lost when the browser tab is closed)
+
+// ---------------------------------------------------------------- [ Check Browser Support ] -----------
+// check browser support for localStorage and sessionStorage
+if (typeof (Storage) !== "undefined") {
+    // Code for localStorage/sessionStorage.
+} else {
+    // Sorry! No Web Storage support..
+}
+
+// Name/value pairs are always stored as strings. Remember to convert them to another format when needed!
+
+// ---------------------------------------------------------------- [ The localStorage Object ] ---------
+// Store
+localStorage.setItem("lastname", "Fred");
+localStorage.lastname = "Fred";
+localStorage["lastname"] = "Fred";
+// Retrieve
+console.log(localStorage.getItem("lastname"));
+console.log(localStorage.lastname);
+console.log(localStorage["lastname"]);
+// Remove
+localStorage.removeItem("lastname");
+
+
+// ---------------------------------------------------------------- [ The SessionStorage Object ] -------
+// The sessionStorage object is equal to the localStorage object, 
+// except that it stores the data for only one session. 
+// The data is deleted when the user closes the specific browser tab.
+// Store
+sessionStorage.setItem("lastname", "Fred");
+sessionStorage.lastname = "Fred";
+// Retrieve
+console.log(sessionStorage.getItem("lastname"));
+console.log(sessionStorage.lastname);
+console.log(sessionStorage["lastname"]);
+// Remove
+sessionStorage.removeItem("lastname");
