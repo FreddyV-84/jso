db94df14e416792509ca13d69d74f3955491212d
FreddyV <verheecke.frederik@gmail.com>
2017-12-27 20:12:55 +0100

    jquery ui

---
+++ jquery/fred/vallendeZwaarden.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // let swords = $('.sword').css('position', 'relative').animate({
+    //     top: '+=600px'
+    // }, 1000, 'easeInCubic', function () {
+    //     $(this).css('transform', 'rotate(30deg)')
+    // });
+    $('.sword:eq(1)').css('position', 'relative').addClass('rotation');
+});
